df745b5aee8a40f014a5f8e79a5d2ba19fad4a2a
beboyle <beboyle@gmail.com>
2013-06-28 15:25:01 -0400

    js_test_worker_server2.js

---
+++ js_test_worker_server2.js
@@ -0,0 +1,9 @@
+// onmessage gets executed for every postMessage from client
+onmessage = function(e) {
+    // Simply parrot back what worker was told
+
+  setTimeout(function() {
+    postMessage("I'm hard of hearing and slow so I'll shout back: " + e.data.toUpperCase() + " (echoed by 2nd worker 2 secs later)")
+	},2000);
+
+}
